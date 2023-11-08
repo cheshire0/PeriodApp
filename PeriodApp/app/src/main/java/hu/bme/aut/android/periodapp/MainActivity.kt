0c3b94912d25a354b8c82a908a31011a24f05f5f
cheshire0 <seboklili1@gmail.com>
2023-11-08 21:16:14 +0100

    pretty bad

---
+++ PeriodApp/app/src/main/java/hu/bme/aut/android/periodapp/MainActivity.kt
@@ -0,0 +1,116 @@
+package hu.bme.aut.android.periodapp
+
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import hu.bme.aut.android.periodapp.databinding.ActivityMainBinding
+import java.util.Date
+import java.util.Locale
+
+
+class MainActivity : AppCompatActivity() {
+    private val lastDayInCalendar = Calendar.getInstance(Locale.ENGLISH)
+    private val sdf = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
+    private val cal = Calendar.getInstance(Locale.ENGLISH)
+
+    private val currentDate = Calendar.getInstance(Locale.ENGLISH)
+    private val currentDay = currentDate[Calendar.DAY_OF_MONTH]
+    private val currentMonth = currentDate[Calendar.MONTH]
+    private val currentYear = currentDate[Calendar.YEAR]
+
+    private var selectedDay: Int = currentDay
+    private var selectedMonth: Int = currentMonth
+    private var selectedYear: Int = currentYear
+
+    private val dates = ArrayList<Date>()
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(binding.calendarRecyclerView)
+
+        lastDayInCalendar.add(Calendar.MONTH, 6)
+
+        setUpCalendar()
+
+        binding.calendarPrevButton.setOnClickListener {
+            if (cal.after(currentDate)) {
+                cal.add(Calendar.MONTH, -1)
+                if (cal == currentDate)
+                    setUpCalendar()
+                else
+                    setUpCalendar(changeMonth = cal)
+            }
+        }
+
+        binding.calendarNextButton.setOnClickListener {
+            if (cal.before(lastDayInCalendar)) {
+                cal.add(Calendar.MONTH, 1)
+                setUpCalendar(changeMonth = cal)
+            }
+        }
+    }
+
+    private fun setUpCalendar(changeMonth: Calendar? = null) {
+        // first part
+        binding.txtCurrentMonth.text = sdf.format(cal.time)
+        val monthCalendar = cal.clone() as Calendar
+        val maxDaysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+        selectedDay =
+            when {
+                changeMonth != null -> changeMonth.getActualMinimum(Calendar.DAY_OF_MONTH)
+                else -> currentDay
+            }
+        selectedMonth =
+            when {
+                changeMonth != null -> changeMonth[Calendar.MONTH]
+                else -> currentMonth
+            }
+        selectedYear =
+            when {
+                changeMonth != null -> changeMonth[Calendar.YEAR]
+                else -> currentYear
+            }
+        var currentPosition = 0
+        dates.clear()
+        monthCalendar.set(Calendar.DAY_OF_MONTH, 1)
+
+        while (dates.size < maxDaysInMonth) {
+            if (monthCalendar[Calendar.DAY_OF_MONTH] == selectedDay)
+                currentPosition = dates.size
+            dates.add(monthCalendar.time)
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        binding.calendarRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val calendarAdapter = CalendarAdapter(this, dates, currentDate, changeMonth)
+        binding.calendarRecyclerView.adapter = calendarAdapter
+
+        when {
+            currentPosition > 2 -> binding.calendarRecyclerView.scrollToPosition(currentPosition - 3)
+            maxDaysInMonth - currentPosition < 2 -> binding.calendarRecyclerView.scrollToPosition(currentPosition)
+            else -> binding.calendarRecyclerView.scrollToPosition(currentPosition)
+        }
+
+        calendarAdapter.setOnItemClickListener(object : CalendarAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                val clickCalendar = Calendar.getInstance()
+                clickCalendar.time = dates[position]
+                selectedDay = clickCalendar[Calendar.DAY_OF_MONTH]
+            }
+        })
+    }
+
+}
